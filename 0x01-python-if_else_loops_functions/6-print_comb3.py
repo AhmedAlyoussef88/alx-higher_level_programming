cedecd167e1706426d41d90fa067344d4beff9cf
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-03-12 16:10:38 -0700

    my commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for d1 in range(0, 10):
     for d2 in range(d1 + 1, 10):
         if d1 == 8 and d2 == 9:
-            print(f"{}{}".format(d1, d2))
+            print(f"{d1}{d2}")
         else:
-            print("{}{}".format(d1, d2), end=", ")
+            print("{:02d}, ".format(int(str(d1) + str(d2))), end="")
