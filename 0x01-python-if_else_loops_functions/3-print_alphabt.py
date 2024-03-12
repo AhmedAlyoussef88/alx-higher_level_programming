68d84079706a82d0c6b110939efe79da844ff788
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-03-12 15:57:35 -0700

    my commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if chr(character) is not 'q' and chr(character) is not 'e':
-        print("{}".format(chr(character)), end="")
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
