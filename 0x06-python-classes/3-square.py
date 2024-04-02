05f2d8ad7d8e99e8cd83b98d9132275d377970c7
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-04-02 14:31:10 -0700

    my commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,15 +3,17 @@
 
 
 class Square:
-    """Represent a square."""
+    """Represent square."""
 
     def __init__(self, size=0):
-		
-		if not isinstance(size, int):
+        """Initialize square.
+
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """Return area of the square."""
         return (self.__size * self.__size)
