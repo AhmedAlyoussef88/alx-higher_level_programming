1224c6598c2a84bd6862ec9b81f57881d645d91e
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-04-02 13:49:06 -0700

    my commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as i:
-		print("Exception:", i, file = sys.stderr)
-        return False
-    else:
-        return True
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
