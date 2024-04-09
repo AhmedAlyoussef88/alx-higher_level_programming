6541460396698ce43a6f910b441bcf72d0b9ab1c
ahmedalyoussef88 <ahmed.m.alyoussef@gmail.com>
2024-04-08 18:31:59 -0700

    my commit

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,6 +3,7 @@
 
 
 class Rectangle:
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -22,6 +23,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -33,20 +35,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
